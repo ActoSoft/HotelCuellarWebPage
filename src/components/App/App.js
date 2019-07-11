85a2cad9c68a270cfe6720a2dbaf91699986fc57
Topiltzin <nokoayzack@gmail.com>
2019-07-11 11:38:21 -0500

    Router and Sites structure added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../Header';
+import Routes from '../Routes';
 
 function App() {
   return (
     <Router>
       <Header />
       <main>
-        a
+        <Routes />
       </main>
     </Router>
   );

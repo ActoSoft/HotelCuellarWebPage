d117c226dc1a3df528d6674f9dacf1c810ddadcd
Topiltzin <nokoayzack@gmail.com>
2019-07-11 11:29:03 -0500

    Header structure changed

--- src/components/Header/Bot.js
+++ src/components/Header/Bot.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import Search from '../Search';
 import Button from '../Common/Button/Button';
+import { Link } from 'react-router-dom';
 
 const Bot = () => {
   return (
     <div className="bot">
       <Search className="nav" />
       <div className="menu-items">
-        <div className="menu-item">
+        <Link className="menu-item" to="/">
           INICIO
-        </div>
-        <div className="menu-item with-separator">
+        </Link>
+        <Link className="menu-item with-separator" to="/nosotros">
           NOSOTROS
-        </div>
-        <div className="menu-item with-separator">
+        </Link>
+        <Link className="menu-item with-separator" to="/habitaciones">
           HABITACIONES
-        </div>
-        <div className="menu-item with-separator">
+        </Link>
+        <Link className="menu-item with-separator" to="/servicios">
           SERVICIOS
-        </div>
-        <div className="menu-item with-separator">
+        </Link>
+        <Link className="menu-item with-separator" to="/contactanos">
           CONTACTANOS
-        </div>
+        </Link>
       </div>
       <Button className="menu-item">
         RESERVA AHORA

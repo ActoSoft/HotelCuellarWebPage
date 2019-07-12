485fc41c2f26d451c0e84d6108b349d8904ffb0e
Topiltzin <nokoayzack@gmail.com>
2019-07-12 14:43:53 -0500

    BookForm Component added

--- src/components/Header/BookModal.js
+++ src/components/Header/BookModal.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import {Button} from '../Common';
+import BookForm from './BookForm';
 
 const BookModal = ({visible, setBookFormVisible}) => {
   const handleClose = e => {
@@ -12,43 +10,7 @@ const BookModal = ({visible, setBookFormVisible}) => {
 
   return(
     <article id="BookForm" onClick={handleClose} className={`BookForm ${visible ? 'visible' : 'hidden'}`}>
-      <form>
-        <div className="input-control col-xs-full col-half">
-          <label for="fechaEntrada">Fecha de Entrada</label>
-          <DatePicker required id="fechaEntrada" />
-        </div>
-        <div className="input-control col-xs-full col-half">
-          <label for="fechaSalida">Fecha de Salida</label>
-          <DatePicker required id="fechaSalida" />
-        </div>
-        <div className="input-control col-xs-full col-half">
-          <label for="adultos">Adultos</label>
-          <input required type="number" id="adultos" name="adultos" />
-        </div>
-        <div className="input-control col-xs-full col-half">
-          <label for="ninos">Niños</label>
-          <input required type="number" id="ninos" name="ninos" />
-        </div>
-        <div className="input-control col-full">
-          <label for="tipoHabitacion">Tipo de Habitación</label>
-          <select required id="tipoHabitacion" name="tipoHabitacion" >
-            <option selected disabled value="0">Selecciona una habitación</option>
-          </select>
-        </div>
-        <div className="input-control col-full">
-          <label for="nombre">Nombre</label>
-          <input required type="text" id="nombre" name="nombre" />
-        </div>
-        <div className="input-control col-full">
-          <label for="email">Email</label>
-          <input required type="email" id="email" name="email" />
-        </div>
-        <div className="input-control col-full">
-          <Button>
-            Reservar
-          </Button>
-        </div>
-      </form>
+      <BookForm visible={visible} setBookFormVisible={setBookFormVisible} />
     </article>
   )
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,5 @@
 import Header from './Header';
+import BookForm from './BookForm';
 
 export default Header;
+export { BookForm };

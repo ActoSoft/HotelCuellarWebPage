3e074b9e4b1ecff8b782d6c29c78cb97ece777d9
Topiltzin <nokoayzack@gmail.com>
2019-07-29 17:55:33 -0500

    Resolves #8

--- src/components/Sites/Home/Welcome.js
+++ src/components/Sites/Home/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Section, { SectionHeader } from '../../Common/Section';
 import { ImageCard } from '../../Common/Cards';
-import { Typography } from '../../Common';
+import { Typography, Position, Button } from '../../Common';
 import Grid from '../../Common/Grid';
 
 const Welcome = () => {
@@ -25,6 +25,9 @@ const Welcome = () => {
         <Typography align="center" variant="body">
           Fantastic place and what makes it even better is that it is not part of some big international chain but locally owned and run. The rooms were great, if possible try to get a suite as it makes things easier with a young family, the hotel is in such a lovely position right on the beach, approximately 100 paces from sea to pool, a little longer via the bar for either a Piton beer or one of the fantastic cocktails!
         </Typography>
+        <Position align="center">
+          <Button style={{width: '250px', marginTop: '50px'}} dark> MÁS DE NOSOTROS </Button>
+        </Position>
       </SectionHeader>
     </Section>
   );

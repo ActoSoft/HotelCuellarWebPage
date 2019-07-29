e48dd26e8b8b7d479d8d3e04f97035f15291ff97
Topiltzin <nokoayzack@gmail.com>
2019-07-29 18:43:35 -0500

    Resolves #16

--- src/components/Header/Top.js
+++ src/components/Header/Top.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
 
 const Top = () => {
   return (
@@ -6,6 +7,12 @@ const Top = () => {
       <h2>
         Contáctanos directamente al 775 679 3825
       </h2>
+      <div className="social-icons">
+        <a href="facebook.com"><FaFacebookF /></a>
+        <a href="twitter.com"><FaTwitter /></a>
+        <a href="youtube.com"><FaYoutube /></a>
+        <a href="instagram.com"><FaInstagram /></a>
+      </div>
     </div>
   )
 };

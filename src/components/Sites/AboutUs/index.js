130217fddbd58532c2af279050f299d85cb541d1
Topiltzin <nokoayzack@gmail.com>
2019-08-09 14:30:56 -0500

    AboutUs Component added

---
+++ src/components/Sites/AboutUs/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Section, Super } from '../../Common';
+
+const AboutUs = ({}) => {
+  return(
+    <React.Fragment>
+      <Super
+        title="Sobre Nosotros"
+        imgSrc="https://scontent.fmex10-2.fna.fbcdn.net/v/t31.0-8/26962053_2063581077194435_8342261090065595263_o.jpg?_nc_cat=101&_nc_oc=AQklhscY29GcyAfFsOBy-EWa-hveBgDxYzxP0scErQaBoyU3h8H7TPEKA3a0sOzQPus&_nc_ht=scontent.fmex10-2.fna&oh=d5ed810b668f60e5794c1ff38acb3882&oe=5DE548B7" />
+      <Section>
+        
+      </Section>
+    </React.Fragment>
+  )
+};
+
+export default AboutUs;

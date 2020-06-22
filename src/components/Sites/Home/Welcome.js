f7486afc610c713fbdd643739373d550ecc1bc8a
Martín Melo <martin.melo.dev.97@gmail.com>
2020-06-22 10:34:30 -0500

    No c

--- src/components/Sites/Home/Welcome.js
+++ src/components/Sites/Home/Welcome.js
@@ -3,33 +3,33 @@ import Section, { SectionHeader } from '../../Common/Section';
 import { ImageCard } from '../../Common/Cards';
 import { Typography, Position, Button } from '../../Common';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import img from '../../../img/sliders/hotel_back_1_good.jpg'
 
 const Welcome = () => {
   return(
     <Section>
       <Grid>
-        <Row>      
+        <Row>
           <Col xs={12} md={4}>
-            <ImageCard 
+            <ImageCard
               title={'Alojamiento de lujo'}
-              imgSrc={'https://demo.accesspressthemes.com/swing/demo-four/wp-content/uploads/sites/6/2019/06/ac1.jpg'} />
-          </Col>      
+              imgSrc={img} />
+          </Col>
           <Col xs={12} md={4}>
-            <ImageCard 
+            <ImageCard
               title={'Rooftop swimming'}
-              imgSrc={'https://demo.accesspressthemes.com/swing/demo-four/wp-content/uploads/sites/6/2019/06/ac22.jpg'} />
-          </Col>        
+              imgSrc={img} />
+          </Col>
           <Col xs={12} md={4}>
-            <ImageCard 
+            <ImageCard
               title={'Fine wine and dine'}
-              imgSrc={'https://demo.accesspressthemes.com/swing/demo-four/wp-content/uploads/sites/6/2018/02/1-service3.jpg'} />
-          </Col>  
+              imgSrc={img} />
+          </Col>
         </Row>
       </Grid>
       <SectionHeader
         title="Legendary style makes it one of the most prestigious Hotel."
         extra="Fantastic place and what makes it even better is that it is not part of some big international chain but locally owned and run. The rooms were great, if possible try to get a suite as it makes things easier with a young family, the hotel is in such a lovely position right on the beach, approximately 100 paces from sea to pool, a little longer via the bar for either a Piton beer or one of the fantastic cocktails!">
-        
         <Position align="center">
           <Button style={{width: '250px', marginTop: '50px'}} dark> MÁS DE NOSOTROS </Button>
         </Position>
--- src/components/Sites/Home/index.js
+++ src/components/Sites/Home/index.js
@@ -11,6 +11,7 @@ import CostumerWord from './CostumerWord';
 import { FaAngleDoubleUp } from 'react-icons/fa';
 import ContactUs from './ContactUs';
 import Gallery from './Gallery';
+import img from '../../../img/sliders/hotel_back_2.jpg';
 
 const Home = () => {
   const [rooms, setRooms] = useState([
@@ -21,7 +22,7 @@ const Home = () => {
       key: 1,
       imgSrc: 'https://demo.accesspressthemes.com/swing/city-hotel/wp-content/uploads/sites/6/2018/01/po2-1360x800.jpg'
     },
-    
+
     {
       title: 'Deluxe Room',
       pricing: 640.00,
@@ -29,7 +30,7 @@ const Home = () => {
       key: 1,
       imgSrc: 'https://demo.accesspressthemes.com/swing/city-hotel/wp-content/uploads/sites/6/2018/01/po2-1360x800.jpg'
     },
-    
+
     {
       title: 'Deluxe Room',
       pricing: 640.00,
@@ -37,7 +38,7 @@ const Home = () => {
       key: 1,
       imgSrc: 'https://demo.accesspressthemes.com/swing/city-hotel/wp-content/uploads/sites/6/2018/01/po2-1360x800.jpg'
     },
-    
+
     {
       title: 'Deluxe Room',
       pricing: 640.00,
@@ -46,22 +47,22 @@ const Home = () => {
       imgSrc: 'https://demo.accesspressthemes.com/swing/city-hotel/wp-content/uploads/sites/6/2018/01/po2-1360x800.jpg'
     }
   ]);
-  
+
   return(
     <section className="Home">
       <article className="slider">
         <Carousel showIndicators={false} interval={2500} infiniteLoop autoPlay showStatus={false} showThumbs={false}>
           <LazyLoad height={window.innerHeight} offset={400}>
             <React.Fragment>
-              <img alt="slide" src="https://scontent.fmex10-2.fna.fbcdn.net/v/t1.0-9/51424857_2319719334913940_3316925327699083264_o.jpg?_nc_cat=109&_nc_oc=AQnfWsakckNsOi_k_O47WM9mH6FBaQSwO8YB0v6gieGKNor6Eh-AQrgqKRlXGDZ6cvU&_nc_ht=scontent.fmex10-2.fna&oh=7fbe28104a70ca806f8385f8fbd0ebd4&oe=5DA4E0E2" />
+              <img alt="slide" src={img} />
               {/* <p className="legend">Legend 2</p> */}
             </React.Fragment>
           </LazyLoad>
           <LazyLoad height={window.innerHeight} offset={400}>
-            <img alt="slide" src="https://scontent.fmex10-2.fna.fbcdn.net/v/t1.0-9/50788430_2317722425113631_7741016619270275072_o.jpg?_nc_cat=107&_nc_oc=AQnuXr_PMiOavn7WluzqXiEJzaJl7ZEbOSKia-vNrNsL_IxYcRh0wfbe0wROvj4Hv_o&_nc_ht=scontent.fmex10-2.fna&oh=2529d609e41dcc56516fc82105208eea&oe=5DAF0F6E" />
+            <img alt="slide" src={img} />
           </LazyLoad>
           <LazyLoad height={window.innerHeight} offset={400}>
-            <img alt="slide" src="https://scontent.fmex10-2.fna.fbcdn.net/v/t1.0-9/50990872_2314395405446333_7031994351370633216_o.jpg?_nc_cat=106&_nc_oc=AQlltPWlIavdVin8IrpQVdCzZ4zE3MWLhS7cyQFSbmEScFH0pIZtBEWWyBEwZOQPAiM&_nc_ht=scontent.fmex10-2.fna&oh=8c8bb7825362ef691e0ce23acaea3bb6&oe=5DB0447A" />
+            <img alt="slide" src={img} />
           </LazyLoad>
         </Carousel>
         <div className="form-container">
@@ -73,21 +74,21 @@ const Home = () => {
         <Welcome />
         <Rooms rooms={rooms} />
         <CostumersWord >
-          <CostumerWord 
+          <CostumerWord
             imgSrc="https://demo.accesspressthemes.com/swing/city-hotel/wp-content/uploads/sites/6/2017/12/team-member-6-475x345-200x200.jpg"
             desc="Duis metus sem, aliquet vitae mi eget, vehicula vehicula enim. In consectetur velit lectus, sit amet sollicitudin ipsum suscipit sed."
             name="Potry Madonnas"
             contact="Managing Director - AC Lens" >
           </CostumerWord>
-          
-          <CostumerWord 
+
+          <CostumerWord
             imgSrc="https://demo.accesspressthemes.com/swing/city-hotel/wp-content/uploads/sites/6/2017/12/team-member-7-475x345-200x200.jpg"
             desc="In consectetur velit lectus, sit amet sollicitudin ipsum suscipit sed. Integer ut urna sit amet mi commodo aliquam quis in nisl."
             name="John Welson"
             contact="Superviser - Walmart" >
           </CostumerWord>
-          
-          <CostumerWord 
+
+          <CostumerWord
             imgSrc="https://demo.accesspressthemes.com/swing/city-hotel/wp-content/uploads/sites/6/2017/12/team-member-2-475x345-200x200.jpg"
             desc="Duis metus sem, aliquet vitae mi eget, vehicula vehicula enim. In consectetur velit lectus, sit amet sollicitudin ipsum suscipit sed."
             name="Kyara Hussain"
